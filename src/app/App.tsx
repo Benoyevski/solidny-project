b3bacddd3cc6f899e135010acc08c87e18c95ad8
”Ismail” <ibeno95@mail.ru>
2023-02-05 12:04:20 +0300

    выстроил структуру согласно FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, { Suspense, useContext, useState } from "react";
+import React, { Suspense } from "react";
 import "./styles/index.scss";
 import { Routes, Route, Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/config/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 
 
@@ -21,8 +20,8 @@ const {theme, toggleTheme} = useTheme()
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,5 +1,9 @@
 import { useContext } from "react";
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import {
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+  ThemeContext,
+} from "./ThemeContext";
 
 interface UseThemeResult {
   toggleTheme: () => void;
@@ -15,5 +19,5 @@ export function useTheme(): UseThemeResult {
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
-  return {theme, toggleTheme}
+  return { theme, toggleTheme };
 }
