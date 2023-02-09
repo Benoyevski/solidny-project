ac2a3561d26ae511b8205a417136ec65d55302c7
”Ismail” <ibeno95@mail.ru>
2023-02-09 18:22:10 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
     parser: '@typescript-eslint/parser',
@@ -35,6 +36,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'linebreak-style': 0,
         'i18next/no-literal-string': 'off',
+        'max-len': ['error', { ignoreComments: true, code: 120 }],
     },
     globals: {
         __IS_DEV__: true,
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,9 +4,9 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
-            .map(([className, value]) => className),
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
     ]
         .join(' ');
 }
