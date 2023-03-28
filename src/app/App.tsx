094933d8204af0bfb3a28c6403f11b891a9e4ba1
”Ismail” <ibeno95@mail.ru>
2023-03-28 15:29:55 +0300

    fix theme for modal window

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
     const { theme } = useTheme();
 
     return (
-        <div className={classNames('app', {}, [theme])}>
+        <div className={classNames('app', {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -58,7 +58,6 @@ export const Modal = (props: ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     return (
