32a0b2e51a95173f6b3a0319bfe4c218ceaa13ee
”Ismail” <ibeno95@mail.ru>
2023-10-27 22:53:56 +0300

    fix ts lint

--- src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -2,14 +2,14 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { CommentCard } from './CommentCard';
 
 export default {
-   title: 'shared/CommentCard',
-   component: CommentCard,
-   argTypes: {
-      backgroundColor: { control: 'color' },
-   },
+    title: 'shared/CommentCard',
+    component: CommentCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as ComponentMeta<typeof CommentCard>;
 
-const Template: ComponentStory<typeof CommentCard> = (args) => <CommentCard { ...args } />;
+const Template: ComponentStory<typeof CommentCard> = (args) => <CommentCard {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
